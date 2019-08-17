1821743bab7aec54922e402275c056c7bd6ed352
shraddhagsagar <47929431+shraddhagsagar@users.noreply.github.com>
2019-08-17 12:58:38 -0500

    Add files via upload

---
+++ aug17/src/test/java/test/com/CalculatorTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.itexps.Calculator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ShraddhaGanesh
+ */
+ 
+public class CalculatorTest {
+    Calculator cal;
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        cal= new Calculator();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    
+    @Test
+    public void testAdd() {
+        assertEquals(4, cal.add(2, 2));
+    }
+    
+    public void testSub() {
+        assertEquals(6, cal.sub(8, 2));
+    }
+   
+    
+}
